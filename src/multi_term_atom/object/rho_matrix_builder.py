ce9b977e4ab489e906b4f9ee1c22d2f04fa5b0f5
Ivan Yakovkin <yakovkinii@gmail.com>
2026-01-20 20:45:35 +0100

    Release rho_matrix_builder.py

--- src/multi_term_atom/object/rho_matrix_builder.py
+++ src/multi_term_atom/object/rho_matrix_builder.py
@@ -1,48 +1,49 @@
-"""
-TODO
-TODO  This file needs improved documentation.
-TODO
-"""
-
-import logging
 from typing import List, Union
 
 import numpy as np
 from numpy import sqrt
 
+from src.engine.functions.decorators import log_method
 from src.engine.functions.general import half_int_to_str
 from src.engine.functions.looping import PROJECTION, TRIANGULAR
 from src.engine.generators.nested_loops import nested_loops
 from src.multi_term_atom.terms_levels_transitions.level_registry import Term
 
-construct_coherence_id_warned = False
 
-
-def construct_coherence_id(term: Term, K: float, Q: float, J: float, Jʹ: float, term_id:str=None):
-    global construct_coherence_id_warned
-    if term is not None:
-        term_id = term.term_id
-        if not construct_coherence_id_warned:
-            logging.warning("construct_coherence_id: term_id argument is deprecated and will be removed in future versions.")
-            construct_coherence_id_warned = True
-    assert term_id is not None
-    return construct_coherence_id_from_term_id(term_id=term_id, K=K, Q=Q, J=J, Jʹ=Jʹ)
+def construct_coherence_id(term: Term, K: float, Q: float, J: float, Jʹ: float):
+    """
+    Construct a unique ID for a coherence
+    """
+    return construct_coherence_id_from_term_id(term_id=term.term_id, K=K, Q=Q, J=J, Jʹ=Jʹ)
 
 
 def construct_coherence_id_from_term_id(term_id: str, K: float, Q: float, J: float, Jʹ: float):
+    """
+    Construct a unique ID for a coherence
+    """
     return f"{term_id}_K={half_int_to_str(K)}_Q={half_int_to_str(Q)}_J={half_int_to_str(J)}_Jʹ={half_int_to_str(Jʹ)}"
 
 
 class Rho:
     def __init__(self, terms: List[Term]):
+        """
+        A container for the density tensor Rho
+        :param terms: list of all terms.
+        """
         self.data = dict()
         self.terms = terms
 
     def set_from_term_id(self, term_id: str, K: float, Q: float, J: float, Jʹ: float, value: np.ndarray):
+        """
+        Set the value
+        """
         self.data[construct_coherence_id_from_term_id(term_id=term_id, K=K, Q=Q, J=J, Jʹ=Jʹ)] = value
 
-    def __call__(self, K: float, Q: float, J: float, Jʹ: float,  term: Union[Term, str]=None, term_id=None):
-        coherence_id = construct_coherence_id(term=term, term_id=term_id, K=K, Q=Q, J=J, Jʹ=Jʹ)
+    def __call__(self, K: float, Q: float, J: float, Jʹ: float, term: Union[Term, str]) -> float:
+        """
+        Get the value
+        """
+        coherence_id = construct_coherence_id(term=term, K=K, Q=Q, J=J, Jʹ=Jʹ)
         return self.data[coherence_id]
 
 
@@ -50,7 +51,9 @@ class RhoMatrixBuilder:
     def __init__(self, terms: List[Term]):
         """
         This class helps to build the matrix for rhos.
-        All possible rhos are defined by levels.
+        All possible rhos are defined by terms.
+
+        :param terms: list of all terms.
         """
 
         # Create mapping [term_id, K, Q, J, Jʹ] <-> matrix index
@@ -76,16 +79,22 @@ class RhoMatrixBuilder:
 
         # create the matrix
         matrix_size = index
-        n_frequencies = 1
-        self.rho_matrix = np.zeros((n_frequencies, matrix_size, matrix_size), dtype=np.complex128)
+        n_calculations = 1  # this is for future, to calculate rho simultaneously for multiple atmospheric parameters.
+        self.rho_matrix = np.zeros((n_calculations, matrix_size, matrix_size), dtype=np.complex128)
         self.selected_coherence = None
 
+    @log_method
     def reset_matrix(self):
+        """
+        Reset the matrix to fill it from scratch later.
+        """
         self.rho_matrix = self.rho_matrix * 0
 
     def select_equation(self, term: Term, K: int, Q: int, J: float, Jʹ: float):
         """
         Selects the equation to add coefficients to.
+
+        The equation is of a form M_ij rho_j = 0. Here we select i.
         """
         coherence_id = construct_coherence_id(term=term, K=K, Q=Q, J=J, Jʹ=Jʹ)
         self.selected_coherence = coherence_id
@@ -93,6 +102,9 @@ class RhoMatrixBuilder:
     def add_coefficient(self, term: Term, K: int, Q: int, J: float, Jʹ: float, coefficient: np.array):
         """
         Adds a coefficient to the selected equation.
+
+        The equation is of a form M_ij rho_j = 0. We have already selected i.
+        Now we do M_ij += coefficient, where j is defined by {term, K, Q, J, Jʹ}.
         """
 
         if not isinstance(coefficient, np.ndarray):
@@ -104,9 +116,6 @@ class RhoMatrixBuilder:
         coherence_id = construct_coherence_id(term=term, K=K, Q=Q, J=J, Jʹ=Jʹ)
 
         if coefficient.__eq__(0).all():
-            # logging.warning("Coefficient is zero, skipping addition.")
-            # logging.info(f'selected_coherence: {self.selected_coherence}')
-            # logging.info(f'coherence_id: {coherence_id}')
             return
 
         index0 = self.coherence_id_to_index[self.selected_coherence]
@@ -116,6 +125,13 @@ class RhoMatrixBuilder:
         index1 = self.coherence_id_to_index[coherence_id]
         self.rho_matrix[:, index0, index1] += coefficient
 
+    @log_method
     def add_coefficient_from_df(self, df):
+        """
+        Adds a coefficient to the selected equation from the dataframe of the form "index0", "index1", "coefficient".
+
+        The equation is of a form M_ij rho_j = 0.
+        For each df row, we do M_ij += coefficient, where i=index0 and j=index1.
+        """
         df = df[["index0", "index1", "coefficient"]].groupby(["index0", "index1"]).sum().reset_index()
         self.rho_matrix[0, df.index0, df.index1] += df.coefficient
