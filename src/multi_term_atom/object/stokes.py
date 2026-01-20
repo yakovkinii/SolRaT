9a10c76d56f3ed4eabc72412be8a3a826840f2eb
Ivan Yakovkin <yakovkinii@gmail.com>
2026-01-20 18:38:39 +0100

    Release stokes.py.

--- src/multi_term_atom/object/stokes.py
+++ src/multi_term_atom/object/stokes.py
@@ -1,63 +1,20 @@
-"""
-TODO
-TODO  This file needs improved documentation.
-TODO
-"""
-
 import numpy as np
 
 
 class Stokes:
-    """
-    Container class for Stokes parameters (I, Q, U, V) as functions of frequency.
+    def __init__(self, nu: np.ndarray, I: np.ndarray, Q: np.ndarray, U: np.ndarray, V: np.ndarray):  # noqa: E741
+        """
+        Container class for Stokes parameters (I, Q, U, V) as functions of frequency.
 
-    Attributes:
-        nu: Frequency array [Hz]
-        I: Stokes I parameter (intensity)
-        Q: Stokes Q parameter (linear polarization)
-        U: Stokes U parameter (linear polarization)
-        V: Stokes V parameter (circular polarization)
-    """
+        :param nu: Frequency array [Hz]
+        :param I: Stokes I parameter (intensity)
+        :param Q: Stokes Q parameter (linear polarization)
+        :param U: Stokes U parameter (linear polarization)
+        :param V: Stokes V parameter (circular polarization)
+        """
 
-    def __init__(self, nu: np.ndarray, I: np.ndarray, Q: np.ndarray, U: np.ndarray, V: np.ndarray):
         self.nu = nu
-        self.I = I
+        self.I = I  # noqa: E741
         self.Q = Q
         self.U = U
         self.V = V
-
-    def __str__(self):
-        return f"Stokes(nu: {len(self.nu)} points, I: [{self.I.min():.3e}, {self.I.max():.3e}])"
-
-    def copy(self):
-        """Create a deep copy of the Stokes vector"""
-        return Stokes(
-            nu=self.nu.copy(),
-            I=self.I.copy(),
-            Q=self.Q.copy(),
-            U=self.U.copy(),
-            V=self.V.copy()
-        )
-
-    def normalize(self, reference='I_max'):
-        """
-        Normalize Stokes parameters.
-
-        Args:
-            reference: Normalization reference ('I_max', 'I_continuum', or float value)
-        """
-        if reference == 'I_max':
-            norm = self.I.max()
-        elif reference == 'I_continuum':
-            # Use average of first and last points as continuum
-            norm = 0.5 * (self.I[0] + self.I[-1])
-        else:
-            norm = float(reference)
-
-        return Stokes(
-            nu=self.nu,
-            I=self.I / norm,
-            Q=self.Q / norm,
-            U=self.U / norm,
-            V=self.V / norm
-        )
