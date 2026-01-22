1555e27a4c0720e981b5860364b083486a260c4e
Ivan Yakovkin <yakovkinii@gmail.com>
2026-01-22 23:53:50 +0100

    Release angles.py

--- src/multi_term_atom/object/angles.py
+++ src/multi_term_atom/object/angles.py
@@ -1,11 +1,16 @@
-"""
-TODO
-TODO  This file needs improved documentation.
-TODO
-"""
-
 class Angles:
     def __init__(self, chi: float = 0, theta: float = 0, gamma: float = 0, chi_B: float = 0, theta_B: float = 0):
+        """
+        A container for all angles defining the LOS and B directions.
+
+        :param chi: LOS angle chi
+        :param theta: LOS angle theta
+        :param gamma: LOS angle gamma
+        :param chi_B: B angle chi
+        :param theta_B: B angle theta
+
+        Reference: Fig. 5.9.
+        """
         self.chi = chi
         self.theta = theta
         self.gamma = gamma
